be0ce9de574ba9e884432ed08db9836c75e1cdc5
jooseariias <arijoose@gmail.com>
2023-04-24 09:12:34 -0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+
+const PORT = process.env.PORT || 3001
+
+
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
     console.log("%s listening at 3001");
   });
 });
