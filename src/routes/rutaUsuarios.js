8bb2319ceb780bcea01cbcc8f10b1bd0558b69e2
Nacho <ignacio_coletta@hotmail.com>
2023-04-28 11:10:06 -0300

    ruta get de users

--- src/routes/rutaUsuarios.js
+++ src/routes/rutaUsuarios.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
 const { Usuario } = require("../db.js");
+const { Op } = require("sequelize");
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const router = Router();
 
@@ -15,22 +19,87 @@ router.post('/', async (req, res) => {
 
         if(!usuarioExistente){
             const nuevoUsuario = await Usuario.create({
-                nombre,
-                apellido,
-                password,
+                nombre: nombre.toUpperCase(),
+                apellido: apellido.toUpperCase(),
+                password: bcrypt.hashSync(password, 8),
                 email,
                 rol,
                 imagen, 
                 telefono,
                 instagram
             })
+            const token = jwt.sign({ email }, "secret");
 
-            res.status(200).json(nuevoUsuario)
+            return res.status(200).send({ message: "User registered", token });
+            // res.status(200).json(nuevoUsuario)
         }else {
             res.status(400).json({msg: `El usuario con email ${email} ya existe en la base de datos`})
         }
     }catch(error){
         res.status(400).json({error: error.message})
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const {nombre, email} = req.query
+        
+        if(nombre && !email) {
+            const usuario = await Usuario.findAll({
+                where: {
+                    nombre: {
+                        [Op.like]: `%${nombre}%`
+                    }
+                }
+            })
+            if(usuario.length > 0){
+                res.status(200).json(usuario)
+            }else {
+                res.status(400).json({msg: `El usuario con nombre ${nombre} no se encuentra en la basde de datos`})
+            }
+        }
+        if(!nombre && email){
+            const usuario = await Usuario.findAll({
+                where: {
+                    email: {
+                        [Op.like]: `${email}`
+                    }
+                }
+            })
+            if(usuario.length > 0){
+                res.status(200).json(usuario)
+            }else {
+                res.status(400).json({msg: `No se encontro el usuario con email ${email}`})
+            }
+        }
+        if(nombre && email){
+            const usuario = await Usuario.findAll({
+                where: {
+                    email: {
+                        [Op.like]: `${email}`
+                    },
+                    nombre: {
+                        [Op.like]: `%${nombre}%`
+                    }
+                }
+            })
+            if(usuario.length > 0){
+                res.status(200).json(usuario)
+            }else {
+                res.status(400).json({msg: `No se encontro el usuario con nombre ${nombre} y email ${email}`})
+            }
+        }
+        if(!nombre && !email){
+            const usuario = await Usuario.findAll()
+
+            if(usuario.length > 0){
+                res.status(200).json(usuario)
+            }else{
+                res.status(400).json({msg: `No hay usuarios en la base de datos`})
+            }
+        }
+    } catch (error) {
+        res.status(400).json({msg: error.message})   
     }
 })
 
